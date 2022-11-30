5cb8b59fcae9ae83dcf95c4cc5d01da871a543ff
Sakib Rahman <seu.sakib@gmail.com>
2022-12-01 01:24:45 +0600

    -

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5050;
 
 const cors = require("cors");
 
